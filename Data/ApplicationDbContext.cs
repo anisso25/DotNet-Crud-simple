51703db3a6c9d0a97786813583a4fd3947b338d6
anisso25 <rahmoni162@gmail.com>
2024-06-20 21:51:07 +0200

    finish crud

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using CrudDotNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudDotNet.Data 
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
